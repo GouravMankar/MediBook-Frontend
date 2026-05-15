7f6580f93726df55324427a770b10d02ac3f0af7
GouravMankar <gouravmankar@hotmail.com>
2026-05-15 16:11:24 +0530

    [Gourav Mankar]: Update the aws public url.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: "http://localhost:8080",
+  apiBaseUrl: "http://13.60.197.51:8080",
   razorpayKeyId: "rzp_test_ShDsl9I21338Je",
 };
